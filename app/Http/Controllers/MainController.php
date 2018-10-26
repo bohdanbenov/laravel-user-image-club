fde55268c2972c2bd4fd9e84d4e5e4f5ae327f76
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-26 12:58:44 +0300

    Add dynamic changing of views.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,10 +3,49 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use GuzzleHttp\Client;
 
 class MainController extends Controller
 {
     public function index(){
-        return view('pages.index');
+//        $client = new Client();
+//        $res = $client->request('GET', 'https://api.thecatapi.com/v1/images/search?format=json', [
+//            'headers' => [
+//                'Content-Type' => 'application/json',
+//                'X-API-KEY' => '17d94b92-754f-46eb-99a0-65be65b5d18f'
+//            ]
+//        ]);
+//        $msg = json_decode($res->getBody(), true);
+//
+//        return view('pages.index', compact('msg'));
+
+        $img_id = [];
+        $img_url = [];
+        $client = new Client();
+
+        if(auth()->check()){
+            if(auth()->user()->admin) {
+                $msg = 'Hello admin';
+                return view('pages.index', compact('msg'));
+            }
+            else{
+                $msg = "Hello ".auth()->user()->firstname;
+                return view('pages.index', compact('msg'));
+            }
+        }
+        else{
+            for($i = 0; $i < 10; $i++){
+                $response = $client->request('GET', 'https://api.thecatapi.com/v1/images/search?format=json', [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'X-API-KEY' => '17d94b92-754f-46eb-99a0-65be65b5d18f'
+                    ]
+                ]);
+                $resp_arr = json_decode($response->getBody(), true);
+                array_push($img_id, $resp_arr[0]['id']);
+                array_push($img_url, $resp_arr[0]['url']);
+            }
+            return view('pages.index', compact('img_id', 'img_url'));
+        }
     }
 }
