1258fb663bead0aeb04be8d8ff90d38dfa33b351
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-26 12:58:05 +0300

    Change views for admin and non login users.

--- resources/views/registration/create.blade.php
+++ resources/views/registration/create.blade.php
@@ -24,14 +24,15 @@
             </div>
 
             <div class="form-group">
+                <label for="password">Password: </label>
+                <input type="password" class="form-control" id="password" name="password" required>
+            </div>
+
+            <div class="form-group">
                 <label for="password_confirmation">Password Confirmation: </label>
                 <input type="password" class="form-control" id="password_confirmation" name="password_confirmation" required>
             </div>
 
-            <div class="form-group">
-                <label for="password">Password: </label>
-                <input type="password" class="form-control" id="password" name="password" required>
-            </div>
 
             <hr>
 
