3e7be7e73ba2aadf258aa1723e0319e039cc6007
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-30 16:52:44 +0200

    Add ajax query for correct display of random images.

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -8,28 +8,31 @@ use GuzzleHttp\Client;
 class MainController extends Controller
 {
     public function index(){
-        $img_id = [];
-        $img_url = [];
-        $client = new Client();
-
         if(auth()->check()){
             if(auth()->user()->admin) {
                 return view('pages.admin.index');
             }
+            else{
+                return view('pages.index');
+            }
         }
         else{
-            for($i = 0; $i < 10; $i++){
-                $response = $client->request('GET', 'https://api.thecatapi.com/v1/images/search?format=json', [
-                    'headers' => [
-                        'Content-Type' => 'application/json',
-                        'X-API-KEY' => '17d94b92-754f-46eb-99a0-65be65b5d18f'
-                    ]
-                ]);
-                $resp_arr = json_decode($response->getBody(), true);
-                array_push($img_id, $resp_arr[0]['id']);
-                array_push($img_url, $resp_arr[0]['url']);
-            }
-            return view('pages.index', compact('img_id', 'img_url'));
+            return view('pages.index');
         }
+    }
+
+    public function generateRandomImage(Request $request){
+        $client = new Client();
+
+        $response = $client->request('GET', 'https://api.thecatapi.com/v1/images/search?format=json', [
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'X-API-KEY' => '17d94b92-754f-46eb-99a0-65be65b5d18f'
+            ]
+        ]);
+        $resp_arr = json_decode($response->getBody(), true);
+
+
+        return $resp_arr[0]['url'];
     }
 }
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -2,13 +2,48 @@
 
 @section('content')
     <div class="col-sm-8 blog-main">
-        @if(isset($img_id) && isset($img_url))
+        @if(auth()->check())
+            <h1>Profile</h1>
+
+            <ul>
+                <li>Firstname: {{auth()->user()->firstname}}</li>
+                <li>Lastname: {{auth()->user()->lastname}}</li>
+                <li>Email: {{auth()->user()->email}}</li>
+                <li>Age: {{auth()->user()->age}}</li>
+                <li>Sex: {{auth()->user()->sex}}</li>
+                <li>Country: {{auth()->user()->country}}</li>
+                <li>City: {{auth()->user()->city}}</li>
+                <li>Street: {{auth()->user()->street}}</li>
+                <li>Homenumber: {{auth()->user()->homenumber}}</li>
+            </ul>
+        @else
+            <script type="text/javascript">
+                $(document).ready(function() {
+                    for(let i = 0; i < 10; i++){
+                        $.ajax({
+                            headers: {
+                                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                            },
+                            type: 'get',
+                            url: '/randimg',
+                            success: function (data) {
+                                $('#image' + i).attr('src', data);
+                            }
+                        })
+                    }
+                });
+            </script>
+
             <h1>Here 10 great pictures for you</h1>
             <div class="grid">
-                @foreach($img_url as $url)
-                    <img src="{{ $url }}" alt="Random image" class="img-thumbnail">
-                @endforeach
+                @for($i = 0; $i < 10; $i++)
+                    <img src="/img/main_placeholder.png" id='{{'image'.$i}}' alt="Random image" class="img-thumbnail">
+                @endfor
             </div>
         @endif
     </div>
+@endsection
+
+@section('sidebar')
+    @include('layouts.sidebar')
 @endsection
