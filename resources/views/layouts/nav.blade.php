55b5dfbed219322f5e1fda8f97beb5c109eed33f
Bohdan Benovskyi <bohdanben@tahometer.com>
2018-10-30 16:57:40 +0200

    Add button group for 'register-login' buttons.

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,17 +1,18 @@
 <div class="blog-masthead">
     <div class="container">
         <nav class="nav blog-nav">
-            <a class="nav-link active" href="#">Home</a>
-            <a class="nav-link" href="#">New features</a>
-            <a class="nav-link" href="#">Press</a>
-            <a class="nav-link" href="#">New hires</a>
-            <a class="nav-link" href="#">About</a>
+            <a class="nav-link active" href="/">Home</a>
+            @if(Auth::check())
+                <a class="nav-link" href="/images">Images</a>
+            @endif
 
             @if(Auth::check())
-                <a class="btn btn-primary ml-auto" href="/logout">Logout</a>
+                <a class="nav-link btn btn-primary ml-auto" href="/logout">Logout</a>
             @else
-                <a class="btn btn-primary ml-auto" href="/register">Register</a>
-                <a class="btn btn-primary ml-auto" href="/login">Login</a>
+                <div class="nav-link btn-group ml-auto" role="group" aria-label="Basic example">
+                    <a class="btn btn-secondary" href="/register">Register</a>
+                    <a class="btn btn-secondary" href="/login">Login</a>
+                </div>
             @endif
 
         </nav>
